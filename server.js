2e8a7b55f331ca4c6551405dbf9f8a25f862c13f
hodaya55 <hodayah159@gmail.com>
2018-06-12 00:43:33 +0300

    update

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var mongoose = require('mongoose');
 //   console.log("DB connection established!!!");
 // })
 
-mongoose.connect('mongodb://localhost/spacebookDB', { useMongoClient: true }, function () {
-  console.log("DB connection established!!!");
-})
+// mongoose.connect('mongodb://localhost/spacebookDB', { useMongoClient: true }, function () {
+//   console.log("DB connection established!!!");
+// })
 
-// let myConnection = process.env.CONNECTION_STRING || 'mongodb://localhost/spacebookDB'
-// mongoose.connect(myConnection, { useMongoClient: true })
-//   .then(() => {console.log('Successfully connected to mongoDB');})
-//   .catch((error) => console.error(error));
+let myConnection = process.env.CONNECTION_STRING || 'mongodb://localhost/spacebookDB';
+mongoose.connect(myConnection, { useMongoClient: true })
+  .then(() => {console.log('Successfully connected to mongoDB');})
+  .catch((error) => console.error(error));
 
 var Post = require('./models/postModel');
 
